d19e999db9e260e158749ac2e3f0ec0ae02c8012
FrenchKrab <14005967+FrenchKrab@users.noreply.github.com>
2023-05-13 21:21:18 +0200

    add EscapeOnHard and EveryoneGetsExp

---
+++ EscapeOnHard/EscapeOnHardMod.cs
@@ -0,0 +1,73 @@
+﻿// Copyright (c) MatthiewPurple.  All Rights Reserved.  Licensed under the MIT license.  See License.txt in the project root for license information.
+
+using EscapeOnHard;
+using HarmonyLib;
+using Il2Cpp;
+using Il2Cppnewbattle_H;
+using MelonLoader;
+using Random = System.Random;
+
+[assembly: MelonInfo(typeof(EscapeOnHardMod), "Escape on Hard [low odds] (ver. 0.6)", "1.0.0", "Matthiew Purple")]
+[assembly: MelonGame("アトラス", "smt3hd")]
+
+namespace EscapeOnHard;
+public class EscapeOnHardMod : MelonMod
+{
+    private static bool s_temporaryNormalMode = false; // Remembers if the game was on Hard difficulty before switching to Normal
+
+    // Before checking if the player can escape
+    [HarmonyPatch(typeof(nbCalc), nameof(nbCalc.nbCheckEscape))]
+    private class Patch
+    {
+        public static void Prefix()
+        {
+            // If the game is on Hard difficulty
+            if (dds3ConfigMain.cfgGetBit(9u) == 2)
+            {
+                dds3ConfigMain.cfgSetBit(9u, 1); // Switches the game to Normal
+                s_temporaryNormalMode = true; // Remembers that it's only temporary
+            }
+        }
+    }
+
+    // After checking if the player can escape
+    [HarmonyPatch(typeof(nbCalc), nameof(nbCalc.nbCheckEscape))]
+    private class Patch2
+    {
+        public static void Postfix(ref nbMainProcessData_t data, ref int __result)
+        {
+            // If Demi-fiend is the one escaping and he has Fast Retreat
+            if (data.activeunit == 0 && datCalc.datCheckSkillInParty(296) == 1)
+            {
+                __result = 1; // Always escape successfully
+            }
+
+            // If on Normal mode temporarily
+            else if (s_temporaryNormalMode)
+            {
+                // If the escape is supposed to be successful then flip a coin
+                if (__result == 1)
+                {
+                    __result = new Random().Next(0, 2);
+                }
+
+                // Switches back to Hard mode
+                dds3ConfigMain.cfgSetBit(9u, 2);
+            }
+
+            s_temporaryNormalMode = false;
+        }
+    }
+
+    [HarmonyPatch(typeof(datSkillHelp_msg), nameof(datSkillHelp_msg.Get))]
+    private class Patch3
+    {
+        public static void Postfix(ref int id, ref string __result)
+        {
+            if (id == 296)
+            {
+                __result = "Guarantees escape \nduring user's turn."; // New description for Fast Retreat
+            }
+        }
+    }
+}
---
+++ EveryoneGetsExp/EveryoneGetsExpMod.cs
@@ -0,0 +1,67 @@
+﻿// Copyright (c) MatthiewPurple.  All Rights Reserved.  Licensed under the MIT license.  See License.txt in the project root for license information.
+
+using EveryoneGetsExp;
+using HarmonyLib;
+using Il2Cpp;
+using MelonLoader;
+
+[assembly: MelonInfo(typeof(EveryoneGetsExpMod), "Everyone Gets Experience 25% (0.6 ver.)", "1.0.0", "Matthiew Purple")]
+[assembly: MelonGame("アトラス", "smt3hd")]
+
+namespace EveryoneGetsExp;
+public class EveryoneGetsExpMod : MelonMod
+{
+    private static uint[] s_unitExpList = Array.Empty<uint>();
+
+    [HarmonyPatch(typeof(nbResultProcess), nameof(nbResultProcess.nbResultLoad))]
+    private class Patch
+    {
+        public static void Prefix()
+        {
+            // Registers everyone's exp after exp attribution at the end of a battle
+            int teamSize = dds3GlobalWork.DDS3_GBWK.unitwork.Length;
+            s_unitExpList = new uint[teamSize];
+            for (int i = 0; i < teamSize; i++)
+            {
+                s_unitExpList[i] = dds3GlobalWork.DDS3_GBWK.unitwork[i].exp;
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(nbResultProcess), nameof(nbResultProcess.nbResultShutdown))]
+    private class Patch2
+    {
+        public static void Postfix()
+        {
+            // Give passive demons either 25% or 100% exp if they have Watchful
+            for (int i = 0; i < dds3GlobalWork.DDS3_GBWK.unitwork.Length; i++)
+            {
+                if (dds3GlobalWork.DDS3_GBWK.unitwork[i].hp > 0)
+                {
+                    // If a demon didn't get any exp yet (i.e. if it's a passive demon without Watchful)
+                    if (s_unitExpList[i] == dds3GlobalWork.DDS3_GBWK.unitwork[i].exp)
+                    {
+                        datCalc.datAddExp(dds3GlobalWork.DDS3_GBWK.unitwork[i], (int)(nbResultProcess.AllExp * 0.25)); // Get 25% exp
+                    }
+                    // If they didn't get 100% exp and have Watchful
+                    else if (s_unitExpList[i] + nbResultProcess.AllExp != dds3GlobalWork.DDS3_GBWK.unitwork[i].exp && dds3GlobalWork.DDS3_GBWK.unitwork[i].skill.Contains(354))
+                    {
+                        datCalc.datAddExp(dds3GlobalWork.DDS3_GBWK.unitwork[i], (int)Math.Ceiling(nbResultProcess.AllExp * 0.5)); // Get 50% exp (+ vanilla 50% exp)
+                    }
+                }
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(datSkillHelp_msg), nameof(datSkillHelp_msg.Get))]
+    private class Patch3
+    {
+        public static void Postfix(ref int id, ref string __result)
+        {
+            if (id == 354)
+            {
+                __result = "Earn 100% EXP when \nnot participating in battle."; // Overwrites Watchful's skill description
+            }
+        }
+    }
+}
