aa597b71790b6c0e6dbc2a3a1c0b1bab909ae82d
FrenchKrab <14005967+FrenchKrab@users.noreply.github.com>
2023-05-13 21:52:12 +0200

    add configuration to EveryoneGetsXp

--- EveryoneGetsExp/EveryoneGetsExpMod.cs
+++ EveryoneGetsExp/EveryoneGetsExpMod.cs
@@ -11,7 +11,21 @@ using MelonLoader;
 namespace EveryoneGetsExp;
 public class EveryoneGetsExpMod : MelonMod
 {
+    public const string ConfigPath = "ModsCfg/EveryoneGetsExp.toml";
+
     private static uint[] s_unitExpList = Array.Empty<uint>();
+
+    private static MelonPreferences_Category s_cfgCategoryMain = null!;
+    private static MelonPreferences_Entry<float> s_cfgSharedXp = null!;
+    private static MelonPreferences_Entry<float> s_cfgSharedXpWatchful = null!;
+
+    public override void OnInitializeMelon()
+    {
+        s_cfgCategoryMain = MelonPreferences.CreateCategory("EveryoneGetsExp");
+        s_cfgCategoryMain.SetFilePath(ConfigPath);
+        s_cfgSharedXp = s_cfgCategoryMain.CreateEntry<float>("SharedXp", 50.0f, "Shared XP", description: "How much XP goes to the party members, in percents.");
+        s_cfgSharedXpWatchful = s_cfgCategoryMain.CreateEntry<float>("SharedXpWatchful", 50.0f, "Shared XP Watchful", description: "In addition to the default 50% XP given to the team members with Watchful, how much additional XP should they get, in percents.");
+    }
 
     [HarmonyPatch(typeof(nbResultProcess), nameof(nbResultProcess.nbResultLoad))]
     private class Patch
@@ -41,12 +55,12 @@ public class EveryoneGetsExpMod : MelonMod
                     // If a demon didn't get any exp yet (i.e. if it's a passive demon without Watchful)
                     if (s_unitExpList[i] == dds3GlobalWork.DDS3_GBWK.unitwork[i].exp)
                     {
-                        datCalc.datAddExp(dds3GlobalWork.DDS3_GBWK.unitwork[i], (int)(nbResultProcess.AllExp * 0.25)); // Get 25% exp
+                        datCalc.datAddExp(dds3GlobalWork.DDS3_GBWK.unitwork[i], (int)(nbResultProcess.AllExp * s_cfgSharedXp.Value / 100)); // Get 25% exp
                     }
                     // If they didn't get 100% exp and have Watchful
                     else if (s_unitExpList[i] + nbResultProcess.AllExp != dds3GlobalWork.DDS3_GBWK.unitwork[i].exp && dds3GlobalWork.DDS3_GBWK.unitwork[i].skill.Contains(354))
                     {
-                        datCalc.datAddExp(dds3GlobalWork.DDS3_GBWK.unitwork[i], (int)Math.Ceiling(nbResultProcess.AllExp * 0.5)); // Get 50% exp (+ vanilla 50% exp)
+                        datCalc.datAddExp(dds3GlobalWork.DDS3_GBWK.unitwork[i], (int)Math.Ceiling(nbResultProcess.AllExp * s_cfgSharedXpWatchful.Value / 100)); // Get 50% exp (+ vanilla 50% exp)
                     }
                 }
             }
@@ -60,7 +74,7 @@ public class EveryoneGetsExpMod : MelonMod
         {
             if (id == 354)
             {
-                __result = "Earn 100% EXP when \nnot participating in battle."; // Overwrites Watchful's skill description
+                __result = $"Earn {50f + s_cfgSharedXpWatchful.Value}% EXP when \nnot participating in battle."; // Overwrites Watchful's skill description
             }
         }
     }
